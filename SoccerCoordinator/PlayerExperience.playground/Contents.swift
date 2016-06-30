f8ccc3ba119aa3b36ab4dcca2204cfb8a185fa78
Jacob Virgin <theaterquasi@gmail.com>
2016-06-30 11:21:43 -0600

    Two separate playground files, one for height (unsure if it works) and one for just player experience

--- SoccerCoordinator/PlayerExperience.playground/Contents.swift
+++ SoccerCoordinator/PlayerExperience.playground/Contents.swift
@@ -5,127 +5,127 @@
 var players = [
     [
         "name": "Joe Smith",
-        "Height": 42,
-        "Experience": true,
+        "Height": "42",
+        "Experience": "true",
         "Guardians": "Jim and Jan Smith",
         "Guardian letter" : ""
     ],
     [
         "name": "Jill Tanner",
-        "Height": 36,
-        "Experience": true,
+        "Height": "36",
+        "Experience": "true",
         "Guardians": "Ciara Tanner",
         "Guardian letter" : ""
     ],
     [
         "name": "Bill Bon",
-        "Height": 43,
-        "Experience": true,
+        "Height": "43",
+        "Experience": "true",
         "Guardians": "Sara and Jenny Bon",
         "Guardian letter" : ""
     ],
     [
         "name": "Eva Gordon",
-        "Height": 45,
-        "Experience": false,
+        "Height": "45",
+        "Experience": "false",
         "Guardians": "Wendy and Mike Gordon",
         "Guardian letter" : ""
     ],
     [
         "name": "Matt Gill",
-        "Height": 40,
-        "Experience": false,
+        "Height": "40",
+        "Experience": "false",
         "Guardians": "Charles and Sylvia Gill",
         "Guardian letter" : ""
     ],
     [
         "name": "Kimmy Stein",
-        "Height": 41,
-        "Experience": false,
+        "Height": "41",
+        "Experience": "false",
         "Guardians": "Bill and Hillary Stein",
         "Guardian letter" : ""
     ],
     [
         "name": "Sammy Adams",
-        "Height": 45,
-        "Experience": false,
+        "Height": "45",
+        "Experience": "false",
         "Guardians": "Jeff Adams",
         "Guardian letter" : ""
     ],
     [
         "name": "Karl Saygan",
-        "Height": 42,
-        "Experience": true,
+        "Height": "42",
+        "Experience": "true",
         "Guardians": "Heather Bledsoe",
         "Guardian letter" : ""
     ],
     [
         "name": "Suzanne Greenberg",
-        "Height": 44,
-        "Experience": true,
+        "Height": "44",
+        "Experience": "true",
         "Guardians": "Henrietta Dumas",
         "Guardian letter" : ""
     ],
     [
         "name": "Sal Dali",
-        "Height": 41,
-        "Experience": false,
+        "Height": "41",
+        "Experience": "false",
         "Guardians": "Gala Dali",
         "Guardian letter" : ""
     ],
     [
         "name": "Joe Kavalier",
-        "Height": 39,
-        "Experience": false,
+        "Height": "39",
+        "Experience": "false",
         "Guardians": "Sam and Elain Kavalier",
         "Guardian letter" : ""
     ],
     [
         "name": "Ben Finkelstein",
-        "Height": 44,
-        "Experience": false,
+        "Height": "44",
+        "Experience": "false",
         "Guardians": "Aaron and Jill Finkelstein",
         "Guardian letter" : ""
     ],
     [
         "name": "Diego Soto",
-        "Height": 41,
-        "Experience": true,
+        "Height": "41",
+        "Experience": "true",
         "Guardians": "Robin and Sarika Soto",
         "Guardian letter" : ""
     ],
     [
         "name": "Chloe Alaska",
-        "Height": 47,
-        "Experience": false,
+        "Height": "47",
+        "Experience": "false",
         "Guardians": "David and Jamie Alaska",
         "Guardian letter" : ""
     ],
     [
         "name": "Arnold Willis",
-        "Height": 43,
-        "Experience": false,
+        "Height": "43",
+        "Experience": "false",
         "Guardians": "Claire Willis",
         "Guardian letter" : ""
     ],
     [
         "name": "Phillip Helm",
-        "Height": 44,
-        "Experience": true,
+        "Height": "44",
+        "Experience": "true",
         "Guardians": "Thomas Helm and Eva Jones",
         "Guardian letter" : ""
     ],
     [
         "name": "Les Clay",
-        "Height": 42,
-        "Experience": true,
+        "Height": "42",
+        "Experience": "true",
         "Guardians": "Wynonna Brown",
         "Guardian letter" : ""
     ],
     [
         "name": "Herschel Krustofski",
-        "Height": 45,
-        "Experience": true,
+        "Height": "45",
+        "Experience": "true",
         "Guardians": "Hyman and Rachel Krustofski",
         "Guardian letter" : ""
     ]
@@ -133,23 +133,20 @@ var players = [
 
 //Create arrays of dictionaries to sort the experienced and unexperienced players seperately
 
-var experiencedPlayers : [Dictionary<String, AnyObject>]
-var otherPlayers : [Dictionary<String, AnyObject>]
-var playerAvgHeight : Double
+var experiencedPlayers : [Dictionary<String, String>] = [["" : ""]]
+var otherPlayers: [Dictionary<String, String>] = [["" : ""]]
 
-for var player in players {
+for player in players {
     
-    if player["Experienced"] == true {
+    if player["Experienced"]! == "true" {
         experiencedPlayers.append(player)
     } else {
         otherPlayers.append(player)
     }
     
-    playerAvgHeight += player["height"]
     
 }
 
-playerAvgHeight /= players.count
 
 //Save values of the team practice times
 let dragonsPracticeTime = "March 17, at 1 PM"
@@ -157,36 +154,32 @@ let sharksPracticeTime = "March 17, at 3 PM"
 let raptorsPracticeTime = "March 18, at 1 PM"
 
 //Create arrays of dictionaries to hold the three seperate teams
-var teamDragons : [Dictionary<String, AnyObject>]
-var teamSharks: [Dictionary<String, AnyObject>]
-var teamRaptors: [Dictionary<String, AnyObject>]
+var teamDragons: [Dictionary<String, AnyObject>] = [["" : ""]]
+var teamSharks: [Dictionary<String, AnyObject>] = [["" : ""]]
+var teamRaptors: [Dictionary<String, AnyObject>] = [["" : ""]]
 
-//create a base of the average heights for every team
-var dragonAvgHeight : Double = playerAvgHeight
-var sharkAvgHeight : Double = playerAvgHeight
-var raptorAvgHeight : Double = playerAvgHeight
 
-//Player limit is based off of the number of experienced players divided by the number of teams.
-let playerLimit = experiencedPlayers.count / 3
 
 
 //Cycle through every experienced player and sort them into a team
 for player in experiencedPlayers {
-    //player average height for every team calculated with the current player.
-    var newDragonAvgHeight = dragonAvgHeight += player["height"] / teamDragons.count
-    var newSharkAvgHeight = sharkAvgHeight += player["height"] / teamSharks.count
-    var newRaptorAvgHeight = raptorAvgHeight += player["height"] / teamRaptors.count
+    //Player limit is based off of the number of experienced players divided by the number of teams.
+    let playerLimit = experiencedPlayers.count / 3
+
     
-    //Logic to see if the new average height would be more or less than 1.5 in away from current average height for all the other teams
-    if (newDragonAvgHeight >= sharkAvgHeight - 1.5 || newDragonAvgHeight <= sharkAvgHeight + 1.5) && (newDragonAvgHeight >= raptorAvgHeight - 1.5 || newDragonAvgHeight <= raptorAvgHeight + 1.5) && teamDragons.count <= playerLimit {
+    //set the player count to increment as players are added
+    var dragonExpPlayerCount = 0
+    var sharkExpPlayerCount = 0
+    
+    //Logic to add only the playerLimit amount of players to each team
+    if dragonExpPlayerCount <= playerLimit {
         teamDragons.append(player)
-        dragonAvgHeight += player["height"]
-    } else if (newSharkAvgHeight >= raptorAvgHeight - 1.5 || newSharkAvgHeight <= raptorAvgHeight + 1.5) && teamSharks.count <= playerLimit  {
+        dragonExpPlayerCount++
+    } else if sharkExpPlayerCount <= playerLimit {
         teamSharks.append(player)
-        sharkAvgHeight += player["height"]
+        sharkExpPlayerCount++
     } else {
         teamRaptors.append(player)
-        raptorAvgHeight += player["height"]
     }
     
 }
@@ -194,26 +187,23 @@ for player in experiencedPlayers {
 //Same logic as before, but using the unexperienced players numbers instead of experienced players
 for player in otherPlayers {
     let playerLimit = otherPlayers.count / 3
-    var newDragonAvgHeight = dragonAvgHeight += player["height"] / teamDragons.count
-    var newSharkAvgHeight = sharkAvgHeight += player["height"] / teamSharks.count
-    var newRaptorAvgHeight = raptorAvgHeight += player["height"] / teamRaptors.count
+
     
-    if (newDragonAvgHeight >= sharkAvgHeight - 1.5 || newDragonAvgHeight <= sharkAvgHeight + 1.5) && (newDragonAvgHeight >= raptorAvgHeight - 1.5 || newDragonAvgHeight <= raptorAvgHeight + 1.5) && teamDragons.count < playerLimit {
+    var dragonPlayerCount = 0
+    var sharkPlayerCount = 0
+    
+    if dragonPlayerCount <= playerLimit {
         teamDragons.append(player)
-        dragonAvgHeight += player["height"]
-    } else if (newSharkAvgHeight >= raptorAvgHeight - 1.5 || newSharkAvgHeight <= raptorAvgHeight + 1.5) && teamSharks.count < playerLimit  {
+        dragonPlayerCount++
+    } else if sharkPlayerCount <= playerLimit {
         teamSharks.append(player)
-        sharkAvgHeight += player["height"]
+        sharkPlayerCount++
     } else {
         teamRaptors.append(player)
-        raptorAvgHeight += player["height"]
     }
+    
 }
 
-dragonAvgHeight /= teamDragons.count
-sharkAvgHeight /= teamSharks.count
-raptorAvgHeight /= teamRaptors.count
-    
 
 
 //Create the letter to send to every guardian, and save it to the players' "guardian letter" variable in their dictionary.
@@ -231,12 +221,6 @@ for var player in teamRaptors {
     player["Guardian letter"] = "Dear " + (player["Guardians"])! + ", your child " + (player["name"]) + " has been included into the Raptors team!"
     player["Guardian letter"] += "Our very first practice is on " + raptorsPracticeTime + ". Can't wait to see you then!"
 }
-
-
-
-
-
-
 
 
 
